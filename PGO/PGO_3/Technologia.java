931916c2739cc8833a98f2d09f4d82f0db4adef3
s31615 <163758069+s31615@users.noreply.github.com>
2024-06-26 17:07:12 +0200

    Delete PGO directory

--- PGO/PGO_3/Technologia.java
+++
@@ -1,9 +0,0 @@
-public class Technologia {
-    String nazwa;
-    int premia;
-
-    public Technologia(String nazwa, int premia) {
-        this.nazwa = nazwa;
-        this.premia = premia;
-    }
-}
--- PGO/PGO_7/NegativeNumberException.java
+++
@@ -1,5 +0,0 @@
-public class NegativeNumberException extends Exception {
-    public NegativeNumberException(String message) {
-        super(message);
-    }
-}
